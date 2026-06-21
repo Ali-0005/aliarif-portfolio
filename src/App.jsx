3d561d35f8d6833194fd0edfe866acc4be372736
Ali-0005 <aliarif.itu@gmail.com>
2026-06-22 00:08:34 +0500

    page routing fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { useEffect } from 'react'
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom'
 import Home from './features/Home'
 import RealEstateProject from './features/Projects'
 import SocifiyProject from './features/Projects/components/SocifiyProject'
@@ -6,9 +7,22 @@ import OmnichannelProject from './features/Projects/components/OmnichannelProjec
 import RagProject from './features/Projects/components/RagProject'
 import BillingProject from './features/Projects/components/BillingProject'
 
+function ScrollToTop() {
+  const { pathname, hash } = useLocation()
+
+  useEffect(() => {
+    if (!hash) {
+      window.scrollTo({ top: 0, left: 0, behavior: 'instant' })
+    }
+  }, [pathname, hash])
+
+  return null
+}
+
 function App() {
   return (
     <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects/real-estate" element={<RealEstateProject />} />
